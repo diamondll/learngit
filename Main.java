cfd15a1996c0730afcf8ef177b83f602a37519d3
AIanswer <465880265@qq.com>
2021-08-31 20:02:21 +0800

    lasdf

---
+++ Main.java
@@ -0,0 +1,11 @@
+package alifj;
+
+/**
+ * @author laodi@onlying.cn
+ * @date 2021/8/31 20:01
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("sdaf");
+    }
+}
