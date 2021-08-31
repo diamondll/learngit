2078bb47faa09eb16fde20914f4a2f009c22e551
AIanswer <465880265@qq.com>
2021-08-31 20:15:49 +0800

    lasdfsa

--- Main.java
+++ Main.java
@@ -6,6 +6,6 @@ package alifj;
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("sdafddsaas");
+        System.out.println("sdkljafddsadsas");
     }
 }
